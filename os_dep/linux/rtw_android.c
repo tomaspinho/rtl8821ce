287e06617aced19404c0952ec587bfc93a0f27d3
Tomás Pinho <me@tomaspinho.com>
2019-08-13 15:38:05 +0100

    Fix build

--- os_dep/linux/rtw_android.c
+++ os_dep/linux/rtw_android.c
@@ -662,7 +662,7 @@ int rtw_android_priv_cmd(struct net_device *net, struct ifreq *ifr, int cmd)
 		goto exit;
 	}
 
-#if (LINUX_KERNEL_VERSION >= KERNEL_VERSION(5,0,0))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,0,0))
 	if (!access_ok(priv_cmd.buf, priv_cmd.total_len)) {
 #else
 	if (!access_ok(VERIFY_READ, priv_cmd.buf, priv_cmd.total_len)) {

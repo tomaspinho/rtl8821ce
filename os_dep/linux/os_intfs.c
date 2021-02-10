a20593ed4ccf2502361dc1cfe3a8b519a275ddb5
Suleyman Poyraz <zaryob.dev@gmail.com>
2021-02-10 20:20:58 +0300

    Fixes for RHEL and CENTOS compile
    RHEL and CENTOS made some upstream patches for 4.18, thats why I make this patch.

--- os_dep/linux/os_intfs.c
+++ os_dep/linux/os_intfs.c
@@ -1327,7 +1327,7 @@ unsigned int rtw_classify8021d(struct sk_buff *skb)
 }
 
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,19,0))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,19,0)) || defined(RHEL_RELEASE_CODE) || defined(CENTOS_RELEASE_CODE)
 static u16 rtw_select_queue(struct net_device *dev, struct sk_buff *skb
     , struct net_device *sb_dev
     #if (LINUX_VERSION_CODE < KERNEL_VERSION(5,2,0))

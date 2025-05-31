b37bfacefbcbca2953d1b298a8ea08f048151d47
Sizhe Zhao <prc.zhao@outlook.com>
2025-06-01 03:38:52 +0800

    fix: use timer_delete_sync for 6.15

--- include/osdep_service_linux.h
+++ include/osdep_service_linux.h
@@ -381,7 +381,11 @@ __inline static void _set_timer(_timer *ptimer, u32 delay_time)
 
 __inline static void _cancel_timer(_timer *ptimer, u8 *bcancelled)
 {
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 15, 0))
+	*bcancelled = timer_delete_sync(&ptimer->timer) == 1 ? 1 : 0;
+#else
 	*bcancelled = del_timer_sync(&ptimer->timer) == 1 ? 1 : 0;
+#endif
 }
 
 static inline void _init_workitem(_workitem *pwork, void *pfunc, void *cntx)
